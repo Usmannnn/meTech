d12cd75d0c2aa952fc8634871c77ad95198e6d9d
osman.oruc <osman.oruc@creative-soft.com.tr>
2022-10-01 17:01:13 +0300

    qr generator

--- src/screens/QRPage.tsx
+++ src/screens/QRPage.tsx
@@ -1,16 +1,16 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, useWindowDimensions, View } from 'react-native'
 import React, { useState, } from 'react'
 import { useNavigation, useTheme } from '@react-navigation/native'
 import QrEdges from '../components/atoms/QrEdges'
 import CustomButton from '../components/molecules/CustomButton'
 import Ionicons from '@expo/vector-icons/Ionicons';
-
+import SvgQRCode from 'react-native-qrcode-svg';
 
 const QRPage = () => {
 
     const { colors } = useTheme()
     const navigation = useNavigation()
-
+    const { height } = useWindowDimensions()
     const [status, setStatus] = useState(true)
 
 
@@ -21,7 +21,7 @@ const QRPage = () => {
                     <Text style={{ color: colors.black, fontWeight: "bold", fontSize: 25, textAlign: "center" }}>Hoş Geldiniz</Text>
                     <Text style={{ color: colors.black, fontSize: 19, textAlign: "center", marginTop: 15 }}>MKolay Kantin'e giriş yapmak için QR kodu turnikeye okutman gerekiyor</Text>
                 </View>
-                <View style={[styles.qrContainer, { width: "85%" }]}>
+                <View style={styles.qrContainer}>
                     <QrEdges
                         edges={[1, 0, 0, 1]}
                         position={[0, undefined, undefined, 0]}
@@ -38,21 +38,20 @@ const QRPage = () => {
                         edges={[0, 0, 1, 1]}
                         position={[undefined, undefined, 0, 0]}
                     />
-
-                    <View style={{ backgroundColor: "black", width: "75%", aspectRatio: 1 }} />
+                    <SvgQRCode value='value' size={height * .2} />
                 </View>
-            </View>
-            <View style={styles.buttonContaier}>
-                <CustomButton
-                    text='Alışveriş Geçmişim'
-                    icon={() => <Ionicons name={"card"} size={30} color={colors.secondary} />}
-                    action={() => navigation.navigate("PurgPage")}
-                />
-                <CustomButton
-                    text='Yeni Kart Ekle'
-                    icon={() => <Ionicons name={"card"} size={30} color={colors.secondary} />}
-                    action={() => navigation.navigate("PurgPage")}
-                />
+                <View style={styles.buttonContaier}>
+                    <CustomButton
+                        text='Alışveriş Geçmişim'
+                        icon={() => <Ionicons name={"card"} size={30} color={colors.secondary} />}
+                        action={() => navigation.navigate("PurgPage")}
+                    />
+                    <CustomButton
+                        text='Yeni Kart Ekle'
+                        icon={() => <Ionicons name={"card"} size={30} color={colors.secondary} />}
+                        action={() => navigation.navigate("PurgPage")}
+                    />
+                </View>
             </View>
         </View>
     )
@@ -67,7 +66,6 @@ const styles = StyleSheet.create({
     },
     innerContainer: {
         flex: 1,
-        alignItems: "center",
         justifyContent: "space-between",
         paddingHorizontal: 15,
         paddingTop: 40,
@@ -77,9 +75,11 @@ const styles = StyleSheet.create({
         alignItems: "center",
     },
     qrContainer: {
+        alignSelf: "center",
         alignItems: "center",
         justifyContent: "center",
         aspectRatio: 1,
+        width: "75%"
     },
     buttonContaier: {
         flexDirection: "row",
